c0febae352fa6e7e42641d8d9168381704b9c673
nateippolito <117186920+nateippolito@users.noreply.github.com>
2023-06-07 23:09:45 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,16 +1,17 @@
-import java.awt.*;
 import javax.swing.*;
-public class Main{
-  public static Display f = new Display();
-  public static int w=600;
-  public static int h=400;
-  public static void main(String[] args){
-    f.setSize(w,h);
-    f.setResizable(true);
-    f.setVisible(true);
-    f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    f.setTitle("Science Program");
-    f.setLocationRelativeTo(null);
-    f.setAlwaysOnTop(true);
-  }
+import java.io.*;
+public class Main
+{
+public static void main(String[] args)
+{
+  int w = 500;
+	int h = 500;
+		
+JFrame frame = new JFrame("Circle Game - Project by Nate Ippolito");
+frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+frame.getContentPane().add(new Panel(w,h));
+frame.pack();
+frame.setVisible(true);
+frame.setResizable(true);
+}
 }
